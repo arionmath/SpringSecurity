9faa850b1e8c6ae49be0f68bfc455b37c7f54e72
Arion Mathias <arion.mathias@gmail.com>
2021-08-05 20:25:28 -0300

    update respositorie

--- src/main/java/com/SpringSecurity/normalthings/repositorie/RepositorieStudent.java
+++ src/main/java/com/SpringSecurity/normalthings/repositorie/RepositorieStudent.java
@@ -1,24 +1,11 @@
 package com.SpringSecurity.normalthings.repositorie;
 
-import java.util.Arrays;
-import java.util.List;
-
-import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.SpringSecurity.normalthings.models.ModelStudent;
-@Repository
-public class RepositorieStudent {
+
+public interface RepositorieStudent extends JpaRepository<ModelStudent, String>  {
 	
-	public ModelStudent findByName(){
-		System.out.println("\n===findnamen===\n");
-		return new ModelStudent("Jose Alves Cabral", "findById", "8888888");
-	}
 	
-	public List<ModelStudent> findAll(){
-		return Arrays.asList(new ModelStudent("Jose Alves", "findAll", "8888888"),
-				new ModelStudent("Amanda Joana", "findAll", "8888888"),
-				new ModelStudent("mateus goncalves", "findAll", "8888888"),
-				new ModelStudent("Bob Marley", "findAll", "8888888"));
-	}
 
 }
