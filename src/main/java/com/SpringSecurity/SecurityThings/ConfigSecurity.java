343e88643e7e47c767240d597a24375b762a3569
Arion Mathias <arion.mathias@gmail.com>
2021-08-06 12:15:46 -0300

    update configSecurity for CustomUserDetails

--- src/main/java/com/SpringSecurity/SecurityThings/ConfigSecurity.java
+++ src/main/java/com/SpringSecurity/SecurityThings/ConfigSecurity.java
@@ -6,12 +6,18 @@ import org.springframework.security.config.annotation.method.configuration.Enabl
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
+
+import com.SpringSecurity.SecurityThings.service.CustomUserDetailsService;
 
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class ConfigSecurity extends WebSecurityConfigurerAdapter {
+	
+	@Autowired
+	CustomUserDetailsService customUserDetails;
 
 	@Override
 	public void configure(HttpSecurity web) throws Exception {
@@ -24,19 +30,24 @@ public class ConfigSecurity extends WebSecurityConfigurerAdapter {
 		.csrf().disable();
 	}
 
-	@Autowired
-	public void configureGlobal(  AuthenticationManagerBuilder auth) throws Exception {
-		
-		PasswordEncoder encoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
-		auth.inMemoryAuthentication().
-		withUser("administrador").
-		password(encoder.encode("123")).
-		roles("ADMIN")
-		.and()
-		.withUser("batman")
-		.password(encoder.encode("123"))
-		.roles("USER");
-		
+//	@Autowired
+//	public void configureGlobal(  AuthenticationManagerBuilder auth) throws Exception {
+//		
+//		PasswordEncoder encoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
+//		auth.inMemoryAuthentication().
+//		withUser("administrador").
+//		password(encoder.encode("123")).
+//		roles("ADMIN")
+//		.and()
+//		.withUser("batman")
+//		.password(encoder.encode("123"))
+//		.roles("USER");
+//		
+//	}
+	
+	public void configure(  AuthenticationManagerBuilder auth) throws Exception{
+		auth.userDetailsService(customUserDetails).passwordEncoder( new BCryptPasswordEncoder());
 	}
+	
 
 }
